216bab3ce51948b38fc716e87419acefcdca53b1
medinabenjamin <bmedina.m10@gmail.com>
2026-03-11 11:17:03 -0300

    Use new perfil SVG and remove obsolete binary assets

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -20,7 +20,7 @@ const teamMembers = [
   {
     name: "Marcela Salvo Muñoz",
     role: "Analista de Procesos y Estandarización",
-    imageSrc: "/foto_perfil_web.jpg",
+    imageSrc: "/foto_perfil_web.svg",
     imageAlt: "Retrato profesional de Marcela Salvo Muñoz",
     credentials: "Ingeniera Cilvil Industrial Mención Bioprocesos"
   }
